bf3629fc6ba8df3b63269ac8b3cb2846e13a5923
Abdulrahman Mendahawi <67026595+Amendahawi@users.noreply.github.com>
2021-06-19 19:11:27 -0700

    Update CoinCalculator.java

--- CoinCalculator.java
+++ CoinCalculator.java
@@ -8,40 +8,40 @@ public class CoinCalculator {
         //Asking user for the number of nickels.
         System.out.println("Input the count of nickels: ");
         //Reading the users input.
-        int NickelCount = kbReader.nextInt();
+        int nickelCount = kbReader.nextInt();
         //Setting a value for the Nickel.
-        int NickelWorth = 5;
+        int nickelWorth = 5;
         //Calculating the total worth of the Nickel (multiple the amount of Nickels by the value of each Nickel).
-        double TotalNickel = NickelCount*NickelWorth/100.00;
+        double totalNickel = nickelCount*nickelWorth/100.00;
         //Printing total value of the Nickels.
-        System.out.println("Total value of nickels: $" + TotalNickel);
+        System.out.println("Total value of nickels: $" + totalNickel);
 
         //Dimes.
         System.out.println("Input the count of Dime: ");
-        int DimeCount = kbReader.nextInt();
-        int DimeWorth = 10;
-        double TotalDime = DimeCount*DimeWorth/100.00;
-        System.out.println("Total value of dimes: $" + TotalDime);
+        int dimeCount = kbReader.nextInt();
+        int dimeWorth = 10;
+        double totalDime = dimeCount*dimeWorth/100.00;
+        System.out.println("Total value of dimes: $" + totalDime);
 
         //Quarters.
         System.out.println("Input the count of Quarter: ");
-        int QuarterCount = kbReader.nextInt();
-        int QuarterWorth = 25;
-        double TotalQuarter = QuarterCount*QuarterWorth/100.00;
-        System.out.println("Total value of Quarter: $" + TotalQuarter);
+        int quarterCount = kbReader.nextInt();
+        int quarterWorth = 25;
+        double totalQuarter = quarterCount*quarterWorth/100.00;
+        System.out.println("Total value of Quarter: $" + totalQuarter);
 
         //Pennies.
         System.out.println("Input the count of Penny: ");
-        int PennyCount = kbReader.nextInt();
+        int pennyCount = kbReader.nextInt();
         //Closing the Reader.
         kbReader.close();
-        int PennyWorth = 1;
-        double TotalPenny = PennyCount*PennyWorth/100.00;
-        System.out.println("Total value of Penny: $" + TotalPenny);
+        int pennyWorth = 1;
+        double totalPenny = pennyCount*pennyWorth/100.00;
+        System.out.println("Total value of Penny: $" + totalPenny);
 
         //Adding the total value of each coin.
-        double TotalValue = TotalDime + TotalNickel + TotalPenny + TotalQuarter;
+        double totalValue = totalDime + totalNickel + totalPenny + totalQuarter;
         //Printing the combined value of all the coins.
-        System.out.println("$" + TotalValue);
+        System.out.println("$" + totalValue);
     }
 }
